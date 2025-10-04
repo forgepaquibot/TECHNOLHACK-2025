430c065c1ce320d4fe411cac8845e2ec1f5a338d
[Fritz Paquibot] <[fritzpaquibot@gmail.com]>
2025-10-04 17:42:38 -0400

    controller draw map and error code handling. html file is also initialized with the d3 library for the map

--- controller.js
+++ controller.js
@@ -1,7 +1,6 @@
 const MAX_USERS = 30;
 const MAX_USERS_LEADERBOARD = 15;
 const ERR_DUPLICATE_USER = -3
-const d3 = require("d3");
 
 let users = [MAX_USERS];
 let leaderboard = [MAX_USERS_LEADERBOARD];
@@ -26,34 +25,38 @@ function createUser() {
     users.push(newUser); // Add user to the global array
 }
 
-// Load a GeoJSON file of world countries
-d3.json("https://unpkg.com/world-atlas@2/countries-110m.json").then(data => {
-    const countries = topojson.feature(data, data.objects.countries);
-    
-    const width = 800;
-    const height = 400;
-
-    const projection = d3.geoMercator().scale(130).translate([width/2, height/1.5]);
-    const path = d3.geoPath().projection(projection);
-
-    const svg = d3.select("body").append("svg")
-                  .attr("width", width)
-                  .attr("height", height);
-
-    svg.selectAll("path")
-       .data(countries.features)
-       .enter()
-       .append("path")
-       .attr("d", path)
-       .attr("fill", "lightgreen")
-       .attr("stroke", "black");
-});
-
 
 function throwError(errorCode) {
+    switch (errorCode) {
+        case -3:
+            alert("USER ALREADY EXISTS!");
+    }
 
 }
 
 function drawMap() {
+    // Load a GeoJSON file of world countries
+    d3.json("https://unpkg.com/world-atlas@2/countries-110m.json").then(data => {
+        const countries = topojson.feature(data, data.objects.countries);
+        
+        const width = window.innerWidth;
+        const height = window.innerHeight;
 
+        const projection = d3.geoMercator().scale(130).translate([width/2, height/1.5]);
+        const path = d3.geoPath().projection(projection);
+
+        const svg = d3.select("body").append("svg")
+                    .attr("width", width)
+                    .attr("height", height)
+                    .style("display", "block")
+                    .style("margin", "0 auto");
+
+        svg.selectAll("path")
+        .data(countries.features)
+        .enter()
+        .append("path")
+        .attr("d", path)
+        .attr("fill", "lightgreen")
+        .attr("stroke", "black");
+    });
 }
